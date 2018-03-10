8848580e40d6861f503509f243aebf069090fbf3
stormily <773009690@qq.com>
2018-03-10 21:48:28 +0800

    isPalindrd change my return

--- is_palindrome.c
+++ is_palindrome.c
@@ -8,15 +8,12 @@ bool isPalindrome(int x)
 	if (x < 0)
 		return false;
 
-	while (tmp > 0)
-	{
+	while (tmp > 0){
 		re = re * 10 + tmp % 10;
 		tmp = tmp / 10;
 	}
-	if (x == re)
-		return true;
-	else
-		return false;
+	
+	return x == re;
 }
 
 int main()
