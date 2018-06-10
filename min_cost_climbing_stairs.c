1066a42b3ee0ffbd36df0c631f4275b3fa91395d
stormily <773009690@qq.com>
2018-06-10 16:33:29 +0800

    litter_change

--- min_cost_climbing_stairs.c
+++ min_cost_climbing_stairs.c
@@ -8,7 +8,7 @@ int min(int x, int y){
 int minCostClimbingStairs(int* cost, int costSize) {
 	int a[1001];
 	a[0] = cost[0], a[1] = cost[1];
-	for (int i = 2; i<costSize; i++){
+	for (int i = 2; i < costSize; i++) {
 		a[i] = min(a[i - 1], a[i - 2]) + cost[i];
 	}
 	return min(a[costSize - 1], a[costSize - 2]);
