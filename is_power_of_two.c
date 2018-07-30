a5287f8898f13610661504972f783b499db3d44d
stormily <773009690@qq.com>
2018-07-30 11:25:39 +0800

    two

---
+++ is_power_of_two.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+bool isPowerOfTwo(int n) {
+	if (n < 1)
+		return false;
+	while (n != 1) {
+		if (n % 2 == 1) {
+			return false;
+		}
+		n = n / 2;
+	}
+	return true;
+}
+int main()
+{
+	int n = 3;
+	printf("%d\n", isPowerOfTwo(n));
+}
